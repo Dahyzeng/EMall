e71623c7df9f14dbc3e45079abc6670c0abae8aa
Taurin <taurinzeng@aaxischina.com>
2015-12-23 15:54:06 +0800

    first commit

--- src/emall/util/string/constants/AdminConstants.java
+++ src/emall/util/string/constants/AdminConstants.java
@@ -8,4 +8,6 @@ public class AdminConstants {
     public static final String LOGIN_FAIL__LOG_MESSAGE = "登录账户失败";
     public static final String ADD_ADMIN_LOG = "添加管理员:";
     public static final String UPDATE_PASSWORD_LOG = "修改密码";
+    public static final String ADD_CATEGORY_LOG = "添加商品类别：";
+    public static final String DELETE_CATEGORY_LOG = "删除商品类别：";
 }
